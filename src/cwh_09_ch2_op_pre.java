657ce452b1e3162faa75b00599ec7a0af26c7789
hridaynath-patil <hridaynath.patil@zconsolutions.com>
2026-07-31 17:14:27 +0530

    latest

--- src/cwh_09_ch2_op_pre.java
+++ src/cwh_09_ch2_op_pre.java
@@ -25,9 +25,9 @@ public class cwh_09_ch2_op_pre {
         int y = 1;
         //  int k = x * y/2;
 
-        int b = 0;
-        int c = 0;
-        int a = 10;
+        int b = 1;
+        int c = 4;
+        int a = 5;
         int k = b*b - (4*a*c)/(2*a);
         System.out.println(k);
 
