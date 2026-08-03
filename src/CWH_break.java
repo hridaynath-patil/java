7407a20b11f08b89b194797e12ccc0687a60c4d1
hridaynath-patil <hridaynath.patil@zconsolutions.com>
2026-08-03 18:54:22 +0530

    latest

---
+++ src/CWH_break.java
@@ -0,0 +1,11 @@
+public class CWH_break {
+    public static void main(String[] args) {
+        //using for loop
+        for(int i=10;i>0;i--){
+            if(i==7){
+                break;   //break the loop
+            }
+            System.out.println(i);
+        }
+    }
+}
