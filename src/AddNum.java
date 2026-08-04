233ca652020082b6126340cb1700083fcbb198dc
hridaynath-patil <hridaynath.patil@zconsolutions.com>
2026-08-04 12:08:05 +0530

    latest

--- src/AddNum.java
+++
@@ -1,9 +0,0 @@
-public class AddNum {
-    public static void main(String[] args) {
-        System.out.println("Sum of Numbers : ");
-        int num1 = 77;
-        int num2 = 77;
-        int sum = num1 + num2;
-        System.out.println(sum);
-    }
-}
