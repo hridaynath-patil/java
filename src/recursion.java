ae49d62cb297024140890839cf6f912eece0d104
hridaynath-patil <hridaynath.patil@zconsolutions.com>
2026-08-05 16:20:58 +0530

    latest

---
+++ src/recursion.java
@@ -0,0 +1,15 @@
+class recursion {
+    static void rec(int n) {
+        if (n > 0) {
+            // System.out.print("\t"+n); - REVERSE in DESC
+            rec(n - 1);
+            System.out.print("\t"+n);// in ASC
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 12;
+        System.out.print("Numbers from 1 to " + n + ":");
+        rec(n);
+    }
+}
