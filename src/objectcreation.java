22b16e46fe3fa864e1e835c18c01028993a99c94
hridaynath-patil <hridaynath.patil@zconsolutions.com>
2026-08-05 12:36:33 +0530

    latest

---
+++ src/objectcreation.java
@@ -0,0 +1,13 @@
+public class objectcreation {
+
+    static class calculate{
+        int sum(int a, int b){
+            return a+b;
+        }
+    }
+    public static void main(String[] args) {
+        calculate obj = new calculate();
+        int c = obj.sum(5,4);
+        System.out.println(c);
+    }
+}
