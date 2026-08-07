df487a07841bfc3f72bac4a34ac27e9793b7ca63
hridaynath-patil <hridaynath.patil@zconsolutions.com>
2026-08-07 12:06:47 +0530

    latest

--- src/ConstructorDemo.java
+++ src/ConstructorDemo.java
@@ -2,64 +2,66 @@ class Student {
     String name;
     int id;
 
-    // no-arg constructor
+    //basic constructor
     public Student() {
-        name ="Guest Student";
-        id =1000;
-System.out.println("Default constructor triggered");
+        name = "Guest1";
+        id = 7248;
+        System.out.println("Default constructor");
+
     }
 
-    // parameterized
+    //parameterised
     public Student(String name, int id) {
-        this.name =name;
-      this.id = id;
- System.out.println("Param constructor triggered for: ");
+        this.name = name;
+        this.id = id;
+        System.out.println("Parameterised constructor");
     }
 
-    // copy constructor to clone object
+    //copy constructor
     public Student(Student s) {
-        this.name=s.name;
-        this.id =s.id;
- System.out.println("Copy constructor triggered:");
+        this.name = s.name;
+        this.id = s.id;
+        System.out.println("Copy Constructor trigerred");
     }
 
     public void show() {
-  System.out.println("ID: "+id + " | Name: "+name);
+        System.out.println("ID: " + id + "| Name : " + name);
     }
 }
 
-// private constructor
-class MyUtils {
-    private MyUtils() {
-        // empty private constructor so no instance can be created
+    class MyUtils{
+        private MyUtils(){
+
+        }
+        public static void log(String msg){
+            System.out.println(msg);
+        }
     }
 
-    public static void log(String msg) {
- System.out.println("[SYSTEM LOG] " + msg);
+    class ConstructorDemo{
+        public static void main(String[] args){
+            System.out.println("Testing constructor");
+            System.out.println(" ");
+
+            //defailt
+
+            Student s1 =  new Student();
+            s1.show();
+            System.out.println();
+
+            //parameterised
+            Student s2 = new Student("Hriday Patil", 449799);
+            s2.show();
+            System.out.println();
+
+            //copy constructor
+            Student s3 = new Student(s2);
+            s3.show();
+            System.out.println();
+
+            //private cons.
+            MyUtils.log("Static Metihod caled directly without buitlding an object");
+
+        }
     }
-}
 
-class ConstructorDemo {
-    public static void main(String[] args) {
-    System.out.println("Testing Constructors");
-System.out.println();
-
-  // default
-      Student s1 = new Student();
-       s1.show();
-    System.out.println();
-
-// parameterized
-        Student s2 = new Student("Hridaynath Patil", 4447);
-        s2.show();
-  System.out.println();
-
-//copy constructor
-        Student s3 = new Student(s2);
-        s3.show();
-   System.out.println();
-
-//private constructor
-    MyUtils.log("Static method called directly without building an object.");
-    }
-}
