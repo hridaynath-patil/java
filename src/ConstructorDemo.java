b0cb40352e34dd0d730fd16524fca80f0edbdb23
hridaynath-patil <hridaynath.patil@zconsolutions.com>
2026-08-07 11:44:02 +0530

    latest

--- src/ConstructorDemo.java
+++ src/ConstructorDemo.java
@@ -6,25 +6,25 @@ class Student {
     public Student() {
         name ="Guest Student";
         id =1000;
-        System.out.println("Default constructor triggered");
+System.out.println("Default constructor triggered");
     }
 
     // parameterized
     public Student(String name, int id) {
         this.name =name;
         this.id = id;
-        System.out.println("Param constructor triggered for: ");
+ System.out.println("Param constructor triggered for: ");
     }
 
     // copy constructor to clone object
     public Student(Student s) {
         this.name=s.name;
         this.id =s.id;
-        System.out.println("Copy constructor triggered:");
+ System.out.println("Copy constructor triggered:");
     }
 
     public void show() {
-        System.out.println("ID: "+id + " | Name: "+name);
+  System.out.println("ID: "+id + " | Name: "+name);
     }
 }
 
@@ -35,31 +35,31 @@ class MyUtils {
     }
 
     public static void log(String msg) {
-        System.out.println("[SYSTEM LOG] " + msg);
+ System.out.println("[SYSTEM LOG] " + msg);
     }
 }
 
 class ConstructorDemo {
     public static void main(String[] args) {
-        System.out.println("Testing Constructors");
-        System.out.println();
+    System.out.println("Testing Constructors");
+System.out.println();
 
-        // default
-        Student s1 = new Student();
-        s1.show();
-        System.out.println();
+  // default
+      Student s1 = new Student();
+       s1.show();
+    System.out.println();
 
-        // parameterized
+// parameterized
         Student s2 = new Student("Hridaynath Patil", 4447);
         s2.show();
-        System.out.println();
+  System.out.println();
 
-        // 3. copy constructor
+//copy constructor
         Student s3 = new Student(s2);
         s3.show();
-        System.out.println();
+   System.out.println();
 
-        // 4. private constructor
-        MyUtils.log("Static method called directly without building an object.");
+//private constructor
+    MyUtils.log("Static method called directly without building an object.");
     }
 }
