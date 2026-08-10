9b7183028d1e5ba8701290ebe1ebbb9dc3c6b9cc
hridaynath-patil <hridaynath.patil@zconsolutions.com>
2026-08-10 18:26:32 +0530

    latest

---
+++ src/encapsulation.java
@@ -0,0 +1,20 @@
+class encap {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
+
+public class encapsulation{
+    public static void main(String[] args) {
+        encap e =  new encap();
+        e.setName("Hridaynath");
+        System.out.println("Name is : " + e.getName());
+    }
+}
