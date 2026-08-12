85aac4d9229b40ea393b5bd98f0c6893d6ac651a
hridaynath-patil <hridaynath.patil@zconsolutions.com>
2026-08-12 18:50:26 +0530

    latest

---
+++ src/inteface1.java
@@ -0,0 +1,16 @@
+interface testInterface {
+    int a = 10;
+    void display();
+}
+
+class TestClass implements testInterface {
+    public void display(){System.out.println("PAtil");}
+}
+
+class interface1{
+    public static void main(String[] args){
+        TestClass t = new TestClass();
+        t.display();
+        System.out.println(t.a);
+    }
+}
