d172627b003c093a15d377489ac1e21ebec91c58
hridaynath-patil <hridaynath.patil@zconsolutions.com>
2026-08-12 18:32:06 +0530

    latest

---
+++ src/partialabstractionclasses.java
@@ -0,0 +1,17 @@
+abstract class Vehicles {
+    void startEngine() {System.out.println("engine started");}
+    abstract void accelerate();
+}
+
+class Cars extends Vehicles {
+    @Override
+    void accelerate() {System.out.println("car accelerates");}
+}
+
+public class partialabstractionclasses{
+    public static void main(String[] args) {
+        Vehicles v = new Cars();
+        v.startEngine();
+        v.accelerate();
+    }
+}
